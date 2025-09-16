23d8d2d14f3193a2aebb27b290b1a953cb5df8a7
Dr. Jacob Bellmund <bellmund@cyberagentur.de>
2025-09-16 21:33:46 +0200

    create folder

--- scrape.R
+++ scrape.R
@@ -8,4 +8,5 @@ req <- request("https://newsapi.org/v2/everything") |>
     apiKey = Sys.getenv("NEWS_API_KEY")
   )
 
+dir.create('data', showWarnings = FALSE)
 req_perform(req, path = paste0("data/", date, ".json"))
